9ad22a343dc831489ec812ce91281265e3618acc
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2026-03-12 22:11:23 -0300

    fix: modificações na estrutura do projeto

--- src/main/java/iff/edu/br/barbearia/BarbeariaApplication.java
+++ src/main/java/iff/edu/br/barbearia/BarbeariaApplication.java
@@ -2,12 +2,20 @@ package iff.edu.br.barbearia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "iff.edu.br.barbearia",
+    "com.barbearia.backend.application",
+    "com.barbearia.backend.infrastructure",
+    "com.barbearia.backend.shared"
+})
+@EnableJpaRepositories(basePackages = "com.barbearia.backend.infrastructure.persistence.repository")
+@EntityScan(basePackages = "com.barbearia.backend.core.entities")
 public class BarbeariaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BarbeariaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BarbeariaApplication.class, args);
+    }
 }
