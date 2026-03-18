6c528fecde207ca348adea243e157127a8303cbc
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2026-03-18 20:57:13 -0300

    fix

--- src/test/java/com/barbearia/backend/infrastructure/web/controller/ClienteControllerTest.java
+++ src/test/java/com/barbearia/backend/infrastructure/web/controller/ClienteControllerTest.java
@@ -7,10 +7,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import iff.edu.br.barbearia.BarbeariaApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -21,8 +22,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ClienteController.class)
-@ContextConfiguration(classes = BarbeariaApplication.class)  // <-- ESSENCIAL!
+@SpringBootTest(classes = BarbeariaApplication.class)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class ClienteControllerTest {
 
     @Autowired
